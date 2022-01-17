ff758a1fa7d1b0ee797d8f3f7d75528e8bdfeb9c
Kenny Zhao <kennyzhao2004@gmail.com>
2022-01-16 22:54:06 -0500

    123

--- HUDChoosenWeapon.java
+++ HUDChoosenWeapon.java
@@ -6,12 +6,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ChoosenWeapon extends Actor
+
+
+
+public class HUDChoosenWeapon extends Actor
 {
     /**
      * Act - do whatever the ChoosenWeapon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootImage pistol = new GreenfootImage("pistol.png");
+    
+    public HUDChoosenWeapon()
+    {
+        setImage(pistol); 
+        pistol.scale(75,75); 
+    }
+    
     public void act()
     {
         // Add your action code here.
