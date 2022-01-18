958c908fd5b434f76c89e9477cd2a75372c8cad9
EthanWooMonkey <92928095+EthanWooMonkey@users.noreply.github.com>
2022-01-18 18:12:51 -0500

    welcome world and intro

--- MapWorld.java
+++ MapWorld.java
@@ -21,19 +21,34 @@ public class MapWorld extends World
         easy = new Button(); 
         medium = new Button();
         hard = new Button();
+        easy.getImage().scale(148, 52); 
+        medium.getImage().scale(148, 52); 
+        hard.getImage().scale(148, 52); 
         GreenfootImage map1 = new GreenfootImage("map.png");
         GreenfootImage map2 = new GreenfootImage("map2.png");
         GreenfootImage map3 = new GreenfootImage("map3.png");
+        GreenfootImage easyTitle = new GreenfootImage("easy.png"); 
+        GreenfootImage mediumTitle = new GreenfootImage("medium.png"); 
+        GreenfootImage hardTitle = new GreenfootImage("hard.png"); 
+        GreenfootImage mapTitle = new GreenfootImage("map select.png"); 
         map1.scale(300, 300); 
         map2.scale(300, 300);
         map3.scale(300, 300);
-        background.drawImage(map1, 150, 350); 
-        background.drawImage(map2, 450, 350);
-        background.drawImage(map3, 750, 350);
+        background.drawImage(map1, 100, 400); 
+        background.drawImage(map2, 450, 400);
+        background.drawImage(map3, 800, 400);
+        background.drawImage(easyTitle, 150, 225); 
+        background.drawImage(mediumTitle, 450, 225);
+        background.drawImage(hardTitle, 850, 225);
+        background.setColor(Color.BLACK);
+        background.drawImage(mapTitle, 400, 100);
+        background.drawRect(99, 399, 301, 301);
+        background.drawRect(449, 399, 301, 301);
+        background.drawRect(799, 399, 301, 301);
         setBackground(background); 
-        addObject(easy, 300, 300); 
-        addObject(medium, 600, 300); 
-        addObject(hard, 900, 300); 
+        addObject(easy, 250, 350); 
+        addObject(medium, 600, 350); 
+        addObject(hard, 950, 350); 
     }
     public void act()
     {
--- WelcomeWorld.java
+++ WelcomeWorld.java
@@ -13,7 +13,8 @@ public class WelcomeWorld extends World
     public WelcomeWorld()
     {            
         super(1200,800, 1);
-        //GreenfootImage title = new GreenfootImage(); //the custom title
+        GreenfootImage title = new GreenfootImage("title.png"); //the custom title
+        title.scale(681, 132); 
         Text start = new Text("START");
         Text instruct = new Text("INSTRUCTIONS"); 
         Gear leftGear = new Gear(true); 
@@ -22,7 +23,7 @@ public class WelcomeWorld extends World
         instructions = new Button(); 
         GreenfootImage background = new GreenfootImage("1808239.jpg"); //the custom background
         //initialize objects and create them 
-        //background.drawImage(title, 390, 220); //set the images and background     
+        background.drawImage(title, 275, 220); //set the images and background     
         addObject(start, 400, 425);
         addObject(instruct, 800, 425); 
         addObject(play, 400, 500); 
